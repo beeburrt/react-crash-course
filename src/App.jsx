23bf3c8267b5f49c642adb36a5be26a08de204e6
Brian Burt <beeburrt@outlook.com>
2023-02-22 23:55:47 -0800

    Add MainHeader + styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
+
+import MainHeader from "./components/MainHeader";
 import PostsList from "./components/PostsList";
 
 function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const showModalHandler = () => {
+    setModalVisible(true);
+  };
+
+  const hideModalHandler = () => {
+    setModalVisible(false);
+  };
 
   return (
-    <main>
-      <PostsList />
-    </main>
-  )
+    <>
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostsList modalVisible={modalVisible} onPostDone={hideModalHandler} />
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,17 +1,38 @@
+import { useState } from "react";
+
 import Post from "./Post";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 import classes from "./posts-list.module.css";
 
-const PostsList = (props) => {
+const PostsList = ({ modalVisible, onPostDone }) => {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  const bodyChangeHandler = (evt) => {
+    setEnteredBody(evt.target.value);
+  };
+
+  const authorChangeHandler = (evt) => {
+    setEnteredAuthor(evt.target.value);
+  };
+
   return (
     <>
-    <NewPost />
-    <ul className={classes.posts}>
-        <Post author="Chuck Norris" text="You suck Max" />
+      {modalVisible && (
+        <Modal onClose={onPostDone}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} text={enteredBody} />
         <Post author="Boogie Brown" text="This is the best!" />
-    </ul>
+      </ul>
     </>
-  )
-}
+  );
+};
 
 export default PostsList;
